fb64cd45b63bb2557d94fe8b1816b6fd04d5ada4
LUO <82432210+Hirasawa-Ui@users.noreply.github.com>
2021-04-15 10:30:49 +0800

    Add files via upload
    variable 1

--- test.c
+++ test.c
@@ -30,14 +30,61 @@
 	//printf("%lf\n", d);
 	//return 0;
 //}
+//int main()
+//{
+//	printf("%d\n", sizeof(char));//
+//	printf("%d\n", sizeof(short));//
+//	printf("%d\n", sizeof(int));//
+//	printf("%d\n", sizeof(long));//
+//	printf("%d\n", sizeof(long long));//
+//	printf("%d\n", sizeof(float));//
+//	printf("%d\n", sizeof(double));//
+//	return 0;
+//}
+//int main()
+//{
+//	short age = 20;//向内存申请两个字节=16bit位,存放20
+//	float weight = 95.6;//float 向内存申请4个字节=32bit位存放小数
+//
+//	return 0;
+//}
+
+
+
+
+//全局变量和局部变量
+//int num2 = 20;//全局变量-定义在代码块（{})之外的变量
+//int main()
+//{
+//	int num1 = 10;//局部变量-定义在代码块（{}）内的变量
+//
+//		return  0;
+//}
+
+//全局变量和局部变量的同时存在
+//int a = 100;
+//int main()
+//{
+//	int a = 10;
+//	//局部变量和全局变量的名字建议不要相同-容易误会，产生bug
+//	//当局部变量和全局变量的名字相同的时候，局部变量优先
+//	printf("%d\n", a);
+//		return 0;
+//}
+
+
+//局部变量和全局变量使用范围es
+//int main()
+//{
+//	int a = 10;
+//}
+//printf("%d\n", a);
+//return 0;   //局部变量不能在全局使用
+
+
+int a = 10;
 int main()
 {
-	printf("%d\n", sizeof(char));//
-	printf("%d\n", sizeof(short));//
-	printf("%d\n", sizeof(int));//
-	printf("%d\n", sizeof(long));//
-	printf("%d\n", sizeof(long long));//
-	printf("%d\n", sizeof(float));//
-	printf("%d\n", sizeof(double));//
+	printf("%d\n", a);
 	return 0;
-}
+}//全局变量可以在局部使用
